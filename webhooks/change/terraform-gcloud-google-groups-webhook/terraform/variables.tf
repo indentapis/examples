8583390a5ed8f2100378da917cdc6b744e7767d0
Zach Kodjo <zach.kodjo@indent.com>
2021-07-20 12:53:08 -0400

    Added main terraform module and variables

--- webhooks/change/terraform-gcloud-google-groups-webhook/terraform/variables.tf
+++ webhooks/change/terraform-gcloud-google-groups-webhook/terraform/variables.tf
@@ -9,13 +9,21 @@ EOF
 
 variable "project" {
   description = "Google Cloud Platform project to deploy cluster in."
+<<<<<<< HEAD
   default     = "id-drop-weekly-zkodj"
+=======
+  default     = "granite-functions"
+>>>>>>> 221d11d (Added main terraform module and variables)
   type        = string
 }
 
 variable "bucket" {
   description = "Name of bucket to store function sources in"
+<<<<<<< HEAD
   default     = "indent-gcloud-groups-webhooks"
+=======
+  default     = "indent-internal-webhooks"
+>>>>>>> 221d11d (Added main terraform module and variables)
   type        = string
 }
 
@@ -26,11 +34,15 @@ variable "region" {
 }
 
 variable "webhook_secret" {
+<<<<<<< HEAD
   description = "INDENT_WEBHOOK_SECRET for the indent-gcloud-groups-webhook"
   type        = string
 }
 
 variable "service_account_email" {
   description = "Service account email address for managing Google Groups"
+=======
+  description = "INDENT_WEBHOOK_SECRET for the indent-gcloud-google-groups-webhook"
+>>>>>>> 221d11d (Added main terraform module and variables)
   type        = string
 }
