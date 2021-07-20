3d820632199b53c56a9d9595bc3b5f9723e31626
Zach Kodjo <zach.kodjo@indent.com>
2021-07-20 12:31:13 -0400

    feat(/webhooks/change/terraform-gcloud-google-groups/src): Add dependencies for new google groups terraform webhook [ID-859]

--- webhooks/change/terraform-gcloud-google-groups-webhook/src/index.ts
+++ webhooks/change/terraform-gcloud-google-groups-webhook/src/index.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { AxiosResponse } from 'axios'
 import { Event } from '@indent/types'
 import { GaxiosResponse } from 'gaxios'
@@ -7,10 +8,25 @@ import * as groups from './capabilities/google-groups'
 
 exports['webhook'] = async function handle(req: IRequest, res: Response) {
   const { headers, rawBody } = req
+=======
+import { AxiosResponse } from "axios";
+import { Event } from "@indent/types";
+import { GaxiosResponse } from "gaxios";
+import { verify } from "@indent/webhook";
+import { Request, Response } from "express";
+import { SetPolicyResponse } from "@google-cloud/storage";
+import * as groups from "./capabilities/google-groups";
+
+exports["webhook"] = async function handle(req: IRequest, res: Response) {
+  const { headers, body } = req;
+  const rawBody =
+    process.env.NODE_ENV === "development" ? body : JSON.stringify(body);
+>>>>>>> a32c009 (feat(/webhooks/change/terraform-gcloud-google-groups/src): Add dependencies for new google groups terraform webhook [ID-859])
 
   try {
     await verify({
       secret: process.env.INDENT_WEBHOOK_SECRET,
+<<<<<<< HEAD
       body: rawBody.toString(),
       headers,
     })
@@ -84,11 +100,95 @@ exports['webhook'] = async function handle(req: IRequest, res: Response) {
 async function grantPermission(auditEvent: Event) {
   if (groups.matchEvent(auditEvent)) {
     return await groups.grantPermission(auditEvent)
+=======
+      body: rawBody,
+      headers,
+    });
+  } catch (err) {
+    console.error("@indent/webhook.verify(): failed");
+    console.error(err);
+    return res.status(500).json({ status: { message: err.message } });
+  }
+
+  let events: Array<Event>;
+
+  try {
+    let json = JSON.parse(rawBody);
+
+    events = json.events as Event[];
+  } catch (err) {
+    console.error("JSON.parse(body): failed");
+    console.error(err);
+    return res.status(500).json({ status: { message: err.message } });
+  }
+
+  console.log(`@indent/webhook: received ${events.length} events`);
+  console.log(JSON.stringify(events, null, 2));
+
+  try {
+    await Promise.all(
+      events.map(
+        (
+          auditEvent: Event
+        ): Promise<
+          | void
+          | GaxiosResponse<any>
+          | SetPolicyResponse
+          | AxiosResponse<any>
+          | Status
+        > => {
+          let { actor, event, resources } = auditEvent;
+
+          console.log(
+            `@indent/webhook: ${event} { actor: ${
+              actor.id
+            }, resources: ${JSON.stringify(resources.map((r) => r.id))} }`
+          );
+
+          switch (event) {
+            case "access/grant":
+              return grantPermission(auditEvent, events);
+            case "access/revoke":
+              return revokePermission(auditEvent, events);
+            default:
+              console.log("received unknown event");
+              console.log(auditEvent);
+              return Promise.resolve();
+          }
+        }
+      )
+    );
+  } catch (err) {
+    if (err.response) {
+      let res = err.response;
+
+      if (res.body && res.body.toJSON) {
+        console.error(JSON.stringify(res.body.toJSON()));
+      } else if (res.body) {
+        console.error(JSON.stringify(res.body));
+      } else if (res.data) {
+        console.error(JSON.stringify(res.data));
+      } else {
+        console.error(JSON.stringify(res));
+      }
+    } else {
+      console.error(err);
+    }
+  }
+
+  return res.status(200).json({});
+};
+
+async function grantPermission(auditEvent: Event, allEvents: Event[]) {
+  if (groups.matchEvent(auditEvent)) {
+    return await groups.grantPermission(auditEvent);
+>>>>>>> a32c009 (feat(/webhooks/change/terraform-gcloud-google-groups/src): Add dependencies for new google groups terraform webhook [ID-859])
   }
 
   return {
     code: 404,
     message:
+<<<<<<< HEAD
       'This resource is not supported by the capabilities of this webhook.',
   }
 }
@@ -96,11 +196,21 @@ async function grantPermission(auditEvent: Event) {
 async function revokePermission(auditEvent: Event) {
   if (groups.matchEvent(auditEvent)) {
     return await groups.revokePermission(auditEvent)
+=======
+      "This resource is not supported by the capabilities of this webhook.",
+  };
+}
+
+async function revokePermission(auditEvent: Event, allEvents: Event[]) {
+  if (groups.matchEvent(auditEvent)) {
+    return await groups.revokePermission(auditEvent);
+>>>>>>> a32c009 (feat(/webhooks/change/terraform-gcloud-google-groups/src): Add dependencies for new google groups terraform webhook [ID-859])
   }
 
   return {
     code: 404,
     message:
+<<<<<<< HEAD
       'This resource is not supported by the capabilities of this webhook.',
   }
 }
@@ -111,3 +221,15 @@ type Status = {
   code: number
   message: string
 }
+=======
+      "This resource is not supported by the capabilities of this webhook.",
+  };
+}
+
+type IRequest = Request & { rawBody: string };
+
+type Status = {
+  code: number;
+  message: string;
+};
+>>>>>>> a32c009 (feat(/webhooks/change/terraform-gcloud-google-groups/src): Add dependencies for new google groups terraform webhook [ID-859])
