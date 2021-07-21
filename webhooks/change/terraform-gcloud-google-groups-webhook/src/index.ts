277ef4258a1be46f61eca538cb1cee22024334cb
Zach Kodjo <zach.kodjo@indent.com>
2021-07-21 16:03:41 -0400

    Resolve index.ts merge conflict

--- webhooks/change/terraform-gcloud-google-groups-webhook/src/index.ts
+++ webhooks/change/terraform-gcloud-google-groups-webhook/src/index.ts
@@ -87,59 +87,12 @@ async function grantPermission(auditEvent: Event) {
     return await groups.grantPermission(auditEvent)
   }
 
-  let events: Array<Event>;
-
-  console.log(`@indent/webhook: received ${events.length} events`);
-  console.log(JSON.stringify(events, null, 2));
-
-  try {
-    await Promise.all(
-      events.map(
-        (
-          auditEvent: Event
-        ): Promise<
-          void | GaxiosResponse<any> | AxiosResponse<any> | Status
-        > => {
-          let { actor, event, resources } = auditEvent;
-
-          console.log(
-            `@indent/webhook: ${event} { actor: ${
-              actor.id
-            }, resources: ${JSON.stringify(resources.map((r) => r.id))} }`
-          );
-
-          switch (event) {
-            case "access/grant":
-              return grantPermission(auditEvent);
-            case "access/revoke":
-              return revokePermission(auditEvent);
-            default:
-              console.log("received unknown event");
-              console.log(auditEvent);
-              return Promise.resolve();
-          }
-        }
-      )
-    );
-  } catch (err) {
-    if (err.response) {
-      let res = err.response;
-
-      if (res.body && res.body.toJSON) {
-        console.error(JSON.stringify(res.body.toJSON(), null, 2));
-      } else if (res.body) {
-        console.error(JSON.stringify(res.body, null, 2));
-      } else if (res.data) {
-        console.error(JSON.stringify(res.data, null, 2));
-      } else {
-        console.error(JSON.stringify(res, null, 2));
-      }
-    } else {
-      console.error(err);
-    }
+  return {
+    code: 404,
+    message:
+      'This resource is not supported by the capabilities of this webhook.',
   }
-
-};
+}
 
 async function revokePermission(auditEvent: Event) {
   if (groups.matchEvent(auditEvent)) {
