3053dba81564b404a793cbe393a58db0481b0dc6
Zach Kodjo <zach.kodjo@indent.com>
2021-07-21 16:03:41 -0400

    Resolve index.ts merge conflict

--- webhooks/change/terraform-gcloud-google-groups-webhook/src/index.ts
+++ webhooks/change/terraform-gcloud-google-groups-webhook/src/index.ts
@@ -13,83 +13,6 @@ exports['webhook'] = async function handle(req: IRequest, res: Response) {
       secret: process.env.INDENT_WEBHOOK_SECRET,
       body: rawBody.toString(),
       headers,
-<<<<<<< HEAD
-    });
-  } catch (err) {
-    console.error("@indent/webhook.verify(): failed");
-    console.error(err);
-    return res.status(500).json({ status: { message: err.message } });
-  }
-
-  let events: Array<Event>;
-
-  try {
-    let json = JSON.parse(rawBody);
-
-    events = json.events as Event[];
-  } catch (err) {
-    console.error("JSON.parse(body): failed");
-    console.error(err);
-    return res.status(500).json({ status: { message: err.message } });
-  }
-
-  console.log(`@indent/webhook: received ${events.length} events`);
-  console.log(JSON.stringify(events, null, 2));
-
-  try {
-    await Promise.all(
-      events.map(
-        (
-          auditEvent: Event
-        ): Promise<
-          void | GaxiosResponse<any> | AxiosResponse<any> | Status
-        > => {
-          let { actor, event, resources } = auditEvent;
-
-          console.log(
-            `@indent/webhook: ${event} { actor: ${
-              actor.id
-            }, resources: ${JSON.stringify(resources.map((r) => r.id))} }`
-          );
-
-          switch (event) {
-            case "access/grant":
-              return grantPermission(auditEvent);
-            case "access/revoke":
-              return revokePermission(auditEvent);
-            default:
-              console.log("received unknown event");
-              console.log(auditEvent);
-              return Promise.resolve();
-          }
-        }
-      )
-    );
-  } catch (err) {
-    if (err.response) {
-      let res = err.response;
-
-      if (res.body && res.body.toJSON) {
-        console.error(JSON.stringify(res.body.toJSON(), null, 2));
-      } else if (res.body) {
-        console.error(JSON.stringify(res.body, null, 2));
-      } else if (res.data) {
-        console.error(JSON.stringify(res.data, null, 2));
-      } else {
-        console.error(JSON.stringify(res, null, 2));
-      }
-    } else {
-      console.error(err);
-    }
-  }
-
-  return res.status(200).json({});
-};
-
-async function grantPermission(auditEvent: Event) {
-  if (groups.matchEvent(auditEvent)) {
-    return await groups.grantPermission(auditEvent);
-=======
     })
   } catch (err) {
     console.error('@indent/webhook.verify(): failed')
@@ -161,45 +84,23 @@ async function grantPermission(auditEvent: Event) {
 async function grantPermission(auditEvent: Event) {
   if (groups.matchEvent(auditEvent)) {
     return await groups.grantPermission(auditEvent)
->>>>>>> fe713f88832a5921e98b5298d5d2f20ab1e21b59
   }
 
   return {
     code: 404,
     message:
-<<<<<<< HEAD
-      "This resource is not supported by the capabilities of this webhook.",
-  };
-=======
       'This resource is not supported by the capabilities of this webhook.',
   }
->>>>>>> fe713f88832a5921e98b5298d5d2f20ab1e21b59
 }
 
 async function revokePermission(auditEvent: Event) {
   if (groups.matchEvent(auditEvent)) {
-<<<<<<< HEAD
-    return await groups.revokePermission(auditEvent);
-=======
     return await groups.revokePermission(auditEvent)
->>>>>>> fe713f88832a5921e98b5298d5d2f20ab1e21b59
   }
 
   return {
     code: 404,
     message:
-<<<<<<< HEAD
-      "This resource is not supported by the capabilities of this webhook.",
-  };
-}
-
-type IRequest = Request & { rawBody: string };
-
-type Status = {
-  code: number;
-  message: string;
-};
-=======
       'This resource is not supported by the capabilities of this webhook.',
   }
 }
@@ -210,4 +111,3 @@ type Status = {
   code: number
   message: string
 }
->>>>>>> fe713f88832a5921e98b5298d5d2f20ab1e21b59
