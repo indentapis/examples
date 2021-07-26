8344a58acea296ea4f9e8314037029e74d3c3fb3
Zach Kodjo <zach.kodjo@indent.com>
2021-07-26 17:13:21 -0400

    Created terraform plan for deploying webhook to Google Cloud

---
+++ webhooks/pull/terraform-gcloud-google-groups-pull-webhook/terraform/main.tf
@@ -0,0 +1,17 @@
+resource "google_storage_bucket" "function_bucket" {
+  name = var.bucket
+}
+
+module "google-groups" {
+  source     = "../../../../terraform/function"
+  root_dir   = "${path.module}/.."
+  name       = "indent-google-groups-webhook"
+  region     = var.region
+  bucket     = google_storage_bucket.function_bucket.name
+  source_dir = "/dist"
+
+  environment_variables = {
+    INDENT_WEBHOOK_SECRET = var.indent_webhook_secret
+    GOOGLE_CUSTOMER_ID    = var.google_customer_id
+  }
+}
---
+++ webhooks/pull/terraform-gcloud-google-groups-pull-webhook/terraform/variables.tf
@@ -0,0 +1,39 @@
+variable "google_creds_path" {
+  description = <<EOF
+File containing JSON credentials used to authenticate with Google Cloud and create a cluster.
+Credentials can be downloaded at https://console.cloud.google.com/apis/credentials/serviceaccountkey.
+EOF
+  default     = "./secrets/terraform-deploy-key.json"
+  type        = string
+}
+
+variable "project" {
+  description = "Google Cloud Platform project to deploy cluster in."
+  default     = "id-drop-weekly-zkodj"
+  type        = string
+}
+
+variable "bucket" {
+  description = "Name of bucket to store function sources in"
+  default     = "indent-gcloud-groups-webhooks"
+  type        = string
+}
+
+variable "region" {
+  description = "Region to run functions in"
+  default     = "us-central1"
+  type        = string
+}
+
+variable "indent_webhook_secret" {
+  description = "INDENT_WEBHOOK_SECRET for the indent-gcloud-groups-webhook"
+  type        = string
+  sensitive   = true
+}
+
+variable "google_customer_id" {
+  description = "GOOGLE_CUSTOMER_ID for the indent-gcloud-groups-webhook"
+  default     = ""
+  type        = string
+  sensitive   = true
+}
