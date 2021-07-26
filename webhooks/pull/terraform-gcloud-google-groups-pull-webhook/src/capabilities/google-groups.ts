3bf27fc3ebfa03602b734f4f4683e9faa29b7468
Zach Kodjo <zach.kodjo@indent.com>
2021-07-26 17:19:33 -0400

    Moved getAuth function to separate file

---
+++ webhooks/pull/terraform-gcloud-google-groups-pull-webhook/src/capabilities/google-groups.ts
@@ -0,0 +1,31 @@
+import { readFile } from 'fs/promises'
+import { google } from 'googleapis'
+import { authorize } from '../auth/google'
+
+export async function getAuth() {
+  if (process.env.NODE_ENV !== 'development') {
+    let auth = new google.auth.Compute({
+      serviceAccountEmail: process.env.GCP_SVC_ACCT_EMAIL,
+      scopes: ['https://www.googleapis.com/auth/cloud-identity.groups'],
+    })
+
+    let { token } = await auth.getAccessToken()
+    if (!token) {
+      throw new Error('getAuth: getAccessToken: token not found')
+    }
+
+    let tokenInfo = await auth.getTokenInfo(token)
+    console.log(JSON.stringify({ tokenInfo }))
+
+    return auth
+  }
+
+  try {
+    // Load client secrets from a local file.
+    let content = await readFile('credentials.json')
+    return await authorize(JSON.parse(content.toString()))
+  } catch (err) {
+    console.error('Error loading client secret file', err)
+    throw err
+  }
+}
