7c7eceb8aa1b20a890cabfa28229bb7a13c6f027
Zach Kodjo <zach.kodjo@indent.com>
2021-07-27 12:21:16 -0400

    Removed unnecessary console.logs

--- webhooks/pull/terraform-gcloud-google-groups-pull-webhook/src/index.ts
+++ webhooks/pull/terraform-gcloud-google-groups-pull-webhook/src/index.ts
@@ -8,9 +8,8 @@ import { Resource } from '@indent/types'
 const GOOGLE_CUSTOMER_ID = process.env.GOOGLE_CUSTOMER_ID
 
 async function loadFromGoogleGroups(): Promise<Resource[]> {
-  console.log('Loading data from Google Groups')
+  console.log('Loading data from Google Groups...')
   const auth = await getAuth()
-  console.log('Auth:', auth)
   const service = google.cloudidentity({
     version: 'v1',
     auth,
@@ -22,8 +21,6 @@ async function loadFromGoogleGroups(): Promise<Resource[]> {
     parent: `customers/${GOOGLE_CUSTOMER_ID}`,
     view: 'FULL',
   })
-  console.log('Customer ID:', GOOGLE_CUSTOMER_ID)
-  console.log('Groups:', groups)
 
   const kind = 'google.v1.group'
 
@@ -64,7 +61,6 @@ exports['webhook'] = async function handle(req: IRequest, res: Response) {
     try {
       const resources = await loadFromGoogleGroups()
       console.log('pullUpdate: success: ' + pull.kinds)
-      console.log('Resources:', resources)
       return res.status(200).json({ resources })
     } catch (err) {
       console.log('pullUpdate: error: ' + pull.kinds)
