f422637ae798ce721fe9fed2f90a17ca49e396ed
Zach Kodjo <zach.kodjo@indent.com>
2021-08-16 14:23:21 -0400

    updated auth handling in webhook and removed unused scripts

--- webhooks/change/terraform-aws-okta-webhook/src/index.ts
+++ webhooks/change/terraform-aws-okta-webhook/src/index.ts
@@ -3,19 +3,21 @@ import { verify } from '@indent/webhook'
 import * as Indent from '@indent/types'
 import axios from 'axios'
 
+import { getToken } from './utils/okta-auth'
+
 export const handle: APIGatewayProxyHandler = async function handle(event) {
   try {
     await verify({
       secret: process.env.INDENT_WEBHOOK_SECRET,
       headers: event.headers,
-      body: event.body
+      body: event.body,
     })
   } catch (err) {
     console.error('@indent/webhook.verify(): failed')
     console.error(err)
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: { message: err.message } })
+      body: JSON.stringify({ error: { message: err.message } }),
     }
   }
 
@@ -32,7 +34,7 @@ export const handle: APIGatewayProxyHandler = async function handle(event) {
       console.log(
         `@indent/webhook: ${event} { actor: ${
           actor.id
-        }, resources: ${JSON.stringify(resources.map(r => r.id))} }`
+        }, resources: ${JSON.stringify(resources.map((r) => r.id))} }`
       )
 
       switch (event) {
@@ -50,34 +52,36 @@ export const handle: APIGatewayProxyHandler = async function handle(event) {
 
   return {
     statusCode: 200,
-    body: '{}'
+    body: '{}',
   }
 }
 
-const OKTA_TENANT = process.env.OKTA_TENANT
+const OKTA_DOMAIN = process.env.OKTA_DOMAIN
 const OKTA_TOKEN = process.env.OKTA_TOKEN
 
 async function addUserToGroup({ user, group }) {
+  const { Authorization } = await getToken()
   return await axios({
     method: 'put',
-    url: `https://${OKTA_TENANT}/api/v1/groups/${group}/users/${user}`,
+    url: `https://${OKTA_DOMAIN}/api/v1/groups/${group}/users/${user}`,
     headers: {
+      Authorization,
       Accept: 'application/json',
-      Authorization: `SSWS ${OKTA_TOKEN}`,
-      'Content-Type': 'application/json'
-    }
+      'Content-Type': 'application/json',
+    },
   })
 }
 
 async function removeUserFromGroup({ user, group }) {
+  const { Authorization } = await getToken()
   return await axios({
     method: 'delete',
-    url: `https://${OKTA_TENANT}/api/v1/groups/${group}/users/${user}`,
+    url: `https://${OKTA_DOMAIN}/api/v1/groups/${group}/users/${user}`,
     headers: {
+      Authorization,
       Accept: 'application/json',
-      Authorization: `SSWS ${OKTA_TOKEN}`,
-      'Content-Type': 'application/json'
-    }
+      'Content-Type': 'application/json',
+    },
   })
 }
 
@@ -96,7 +100,7 @@ async function grantPermission(auditEvent: Indent.Event) {
     event,
     actor,
     resources,
-    success: result.status >= 200 && result.status < 300
+    success: result.status >= 200 && result.status < 300,
   })
 }
 
@@ -113,7 +117,7 @@ async function revokePermission(auditEvent: Indent.Event) {
     event,
     actor,
     resources,
-    success: result.status >= 200 && result.status < 300
+    success: result.status >= 200 && result.status < 300,
   })
 }
 
@@ -122,8 +126,8 @@ function getOktaIdFromResources(
   kind: string
 ): string {
   return resources
-    .filter(r => r.kind && r.kind.toLowerCase().includes(kind.toLowerCase()))
-    .map(r => {
+    .filter((r) => r.kind && r.kind.toLowerCase().includes(kind.toLowerCase()))
+    .map((r) => {
       if (r.labels && r.labels.oktaId) {
         return r.labels.oktaId
       }
