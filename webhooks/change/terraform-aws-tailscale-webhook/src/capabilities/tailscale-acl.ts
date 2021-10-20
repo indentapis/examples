65106e89bae3ae722b1aa9c4dd4d31caaeff9378
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-10-20 11:24:58 -0400

    add(webhooks/pull): tailscale terraform config and response handling
    update resource info
    
    feat(webhooks/change): terraform-aws-tailscale-webhook
    
    latest changes before starting debug
    
    debug response handling
    
    start of debugging session
    
    revert huJSON related changes
    
    rename variables per review comments
    
    add space

---
+++ webhooks/change/terraform-aws-tailscale-webhook/src/capabilities/tailscale-acl.ts
@@ -0,0 +1,149 @@
+import axios from 'axios'
+import { AxiosResponse } from 'axios'
+import { Event, Resource } from '@indent/types'
+
+const TAILSCALE_API_KEY = process.env.TAILSCALE_API_KEY
+const TAILSCALE_TAILNET = process.env.TAILSCALE_TAILNET
+
+export const matchEvent = (event: Event): boolean => {
+  return (
+    event.resources.filter((r) =>
+      r.kind?.toLowerCase().includes('tailscale.v1.group')
+    ).length > 0
+  )
+}
+
+export async function grantPermission(auditEvent: Event) {
+  try {
+    const { event, resources } = auditEvent
+    const { email } = getResourceByKind(resources, 'user')
+    const { displayName } = getResourceByKind(resources, 'tailscale.v1.group')
+
+    return await updateTailscaleACL({
+      user: email,
+      group: displayName,
+      event,
+      tailnet: TAILSCALE_TAILNET,
+    })
+  } catch (err) {
+    console.error('@indent/webhook.grantPermission(): failed')
+    console.error(err)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        status: {
+          code: 500,
+          message: err.message,
+          details: err.stack,
+        },
+      }),
+    }
+  }
+}
+
+export async function revokePermission(auditEvent: Event) {
+  try {
+    const { event, resources } = auditEvent
+    const { email } = getResourceByKind(resources, 'user')
+    const { displayName } = getResourceByKind(resources, 'tailscale.v1.group')
+
+    return await updateTailscaleACL({
+      user: email,
+      group: displayName,
+      event,
+      tailnet: TAILSCALE_TAILNET,
+    })
+  } catch (err) {
+    console.error('@indent/webhook.revokePermission(): failed')
+    console.error(err)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        status: {
+          code: 500,
+          message: err.message,
+          details: err.stack,
+        },
+      }),
+    }
+  }
+}
+
+async function updateTailscaleACL({
+  user,
+  group,
+  event,
+}: {
+  user: string
+  group: string
+  tailnet: string
+  event: string
+}) {
+  const tailnetGroup = 'group:' + group
+  const { headers, data } = await getTailscaleACL()
+  console.log('ACL with comments', data.toString())
+  const { etag } = headers
+  let currentACL = data
+
+  if (currentACL) {
+    let currentGroup = currentACL.groups[tailnetGroup]
+    if (currentGroup) {
+      if (event === 'access/grant') {
+        if (currentGroup.includes(user)) {
+          return Promise.resolve()
+        }
+        currentGroup.push(user)
+      } else {
+        currentGroup = currentGroup.filter((u) => u !== user)
+      }
+    } else {
+      console.log(
+        `There are no members of the group ${currentGroup}, adding ${user}...`
+      )
+      currentGroup = [user]
+    }
+    currentACL.groups[tailnetGroup] = currentGroup
+  } else {
+    console.warn('No ACL found on Tailscale response')
+  }
+
+  return await postTailscaleACL({ etag, ACL: currentACL })
+}
+
+async function getTailscaleACL(): Promise<AxiosResponse<any>> {
+  return await axios({
+    method: 'get',
+    url: `https://api.tailscale.com/api/v2/tailnet/${TAILSCALE_TAILNET}/acl`,
+    headers: {
+      'Content-Type': 'application/hujson',
+      Accept: 'application/hujson',
+    },
+    auth: {
+      username: TAILSCALE_API_KEY,
+      password: '',
+    },
+  })
+}
+
+async function postTailscaleACL({ etag, ACL }: { etag: string; ACL: any }) {
+  console.log(`etag: ${etag}`)
+  return await axios({
+    method: 'post',
+    url: `https://api.tailscale.com/api/v2/tailnet/${TAILSCALE_TAILNET}/acl`,
+    headers: {
+      Accept: 'application/hujson',
+      'If-Match': etag,
+    },
+    auth: {
+      username: TAILSCALE_API_KEY,
+      password: '',
+    },
+    data: ACL,
+  })
+}
+
+const getResourceByKind = (resources: Resource[], kind: string): Resource => {
+  return resources.filter(
+    (r) => r.kind && r.kind.toLowerCase().includes(kind.toLowerCase())
+  )[0]
+}
---
+++ webhooks/pull/terraform-aws-tailscale-pull-webhook/src/index.ts
@@ -0,0 +1,113 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult,
+} from 'aws-lambda'
+import { Resource } from '@indent/types'
+import { verify } from '@indent/webhook'
+import axios from 'axios'
+
+export const handle: APIGatewayProxyHandler = async function handle(
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> {
+  try {
+    await verify({
+      secret: process.env.INDENT_WEBHOOK_SECRET,
+      headers: event.headers,
+      body: event.body,
+    })
+  } catch (err) {
+    console.error('@indent/webhook.verify(): failed')
+    console.error(err)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        code: 500,
+        message: err.message,
+        details: err.stack,
+      }),
+    }
+  }
+
+  const body = JSON.parse(event.body)
+  const pull = body as { kinds: string[] }
+
+  if (pull && pull.kinds) {
+    console.log('pullUpdate: attempt: ' + pull.kinds)
+    try {
+      const resourcesAsync = await Promise.all(
+        pull.kinds.map(async (kind: string): Promise<Resource[]> => {
+          if (kind.toLowerCase().includes('tailscale.v1.group')) {
+            return await pullGroups()
+          }
+          return []
+        })
+      )
+      const resources = resourcesAsync.flat()
+      console.log('My Resource: ' + resources[0])
+      console.log('pullUpdate: success: ' + pull.kinds)
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ resources }),
+      }
+    } catch (err) {
+      console.log('pullUpdate: error ' + pull.kinds)
+      console.error(err)
+    }
+  } else {
+    console.warn('webhook received unknown payload')
+    console.warn(JSON.stringify(body))
+  }
+
+  return {
+    statusCode: 200,
+    body: '{}',
+  }
+}
+
+const TAILSCALE_TAILNET = process.env.TAILSCALE_TAILNET
+const TAILSCALE_API_KEY = process.env.TAILSCALE_API_KEY
+
+const pullGroups = async (): Promise<Resource[]> => {
+  const timestamp = new Date().toISOString()
+  const tailscaleGroupResources = await loadFromTailscale({
+    transform: (group) => ({
+      id: [`tailscale/${TAILSCALE_TAILNET}`, group.split(':')[1]].join('/'),
+      kind: 'tailscale.v1.Group',
+      displayName: group.split(':')[1],
+      labels: {
+        ['tailscale/tailnet']: TAILSCALE_TAILNET,
+        timestamp,
+      },
+    }),
+  })
+
+  return tailscaleGroupResources
+}
+
+const loadFromTailscale = async ({
+  transform = (r: any): Resource => r,
+}): Promise<Resource[]> => {
+  console.log('Loading data from Tailscale...')
+
+  // call to axios
+  const response = await axios({
+    method: 'get',
+    url: `https://api.tailscale.com/api/v2/tailnet/${TAILSCALE_TAILNET}/acl`,
+    headers: {
+      Accept: 'application/json',
+    },
+    auth: {
+      username: TAILSCALE_API_KEY,
+      password: '',
+    },
+  })
+
+  const { data: results } = response as any
+  console.log('Tailscale response:', response)
+  console.log('Tailscale groups:', results)
+  // parse response
+  if (results?.groups) {
+    return Object.keys(results.groups).map(transform)
+  }
+}
